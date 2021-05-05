016bc76b9b18db554da09fdbdc79b8ea56e48b38
Dyego Alves <dyego.santos.alves@gmail.com>
2021-05-05 14:47:47 -0300

    Add Secao de Grupos em Estagio2

--- limpaImagemUnsed.js
+++ limpaImagemUnsed.js
@@ -1,9 +1,17 @@
+//Ler o JSON
 let json = require('./game.json');
 let name = new Array()
+//Salva os nomes dos JSON game.json 
 json.resources.resources.forEach(function (o, i) {
     name[i] = o.name
 });
-// readdir.js
+// readdir.js ,
+/**
+ * 1- LER O Diretorio os arquivos do diretorio em files;
+ * 2- Compara FILES lista com NAME JSON nomes e salva em R3 a diferenca;
+ * 3- R3 é a diferenca entre Name JSON e Diretorio lista files;
+ * 4 - Delete somente arquivos .PNG da lista R3
+ */
 var fs = require('fs');
 var domain = require('domain').create();
 fs.readdir('./', function (error, files) {
@@ -11,7 +19,6 @@ fs.readdir('./', function (error, files) {
     files.forEach(function (element, index, array) {
         if (name.indexOf(element) == -1)
             r3.push(element);
-
     });
     r3.forEach(function (element, index, array) {
         var fileExt = element.split('.').pop();
